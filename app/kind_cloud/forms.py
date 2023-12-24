4f98b936b46f23b5b92d4ab6e52fccd2af8ff4c0
Isaiah Mann <isaiahbmann@gmail.com>
2023-12-23 18:33:45 -0800

    Add Bootstrap to the project

--- app/kind_cloud/forms.py
+++ app/kind_cloud/forms.py
@@ -2,6 +2,10 @@ from django.forms import ModelForm
 from .models import Kind
 
 class KindForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(KindForm, self).__init__(*args, **kwargs)
+        self.fields['text'].label = "Kind Words"
+
     class Meta:
         model = Kind
         fields = ['text']
